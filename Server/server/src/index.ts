88853d2b299caaf333757e2dd8d0978aace8f81a
SohamRoyNoel <soham.roy.developer@gmail.com>
2020-12-29 16:52:55 +0530

    Refresh token system implemented

--- Server/server/src/index.ts
+++ Server/server/src/index.ts
@@ -9,16 +9,49 @@ import { UserLoginResolver } from "./resolvers/Login/User_Login.resolver";
 import { ProtectedResolverHealth } from './resolvers/ProtectedResolvers/ProtectedResolver.health';
 import "dotenv/config";
 var morgan = require('morgan');
+import cookieParser = require("cookie-parser");
+import { verify } from 'jsonwebtoken';
+import { User_Registration } from './entity/User_Registration';
+import { CreateAccessToken, CreateRefreshToken } from './utils/tokenCreator';
 
 // Do not use in PRODUCTION: GraphQL Lifecycle logger - DEV only
 import { graphql_REQ_Query_LifeCycle_Logger_dev } from "./utils/graphql_REQ_Query_LifeCycle.Logger.dev";
-
+import { SendRefreshTokenOnRefreshedAccessToken } from './utils/sendRefreshTokenOnRefreshedAccessToken';
 
 (async () => {
     const app = express();
     app.use(morgan('combined'));
-    await createConnection();
 
+    // Handle refresh token
+    app.use(cookieParser());
+    app.post('/getNewAccessToken', async (req, res) => {
+
+        // Perse the cookie
+        const cookieRefreshToken = req.cookies._atr;
+        if(!cookieRefreshToken) { return res.send({"status": false, accessToken: ''}); }
+
+        // Get the TOKEN and decode the payload
+        let payload: any = null;
+        try {
+            payload = verify(cookieRefreshToken, process.env.REFRESH_JWT_SECRET!);
+        } catch (error) {
+            console.log(error);
+            return res.send({"status": false, accessToken: ''});
+        }
+
+        // find the user
+        const userOne = await User_Registration.findOne({ Reg_UserID: payload.uid });
+        if(!userOne) { return res.send({"status": false, accessToken: ''}); }
+
+        // When refresh the Access Token we will refresh the Refresh token
+        SendRefreshTokenOnRefreshedAccessToken(res, CreateRefreshToken(userOne)); 
+
+        // return a Access token
+        return res.send( { "status": true, accessToken: CreateAccessToken(userOne)} );
+    });
+
+    await createConnection();  
+    
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [
