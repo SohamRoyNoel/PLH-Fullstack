14dca5c5a0ff24b5a21c423c47854680fe643781
SohamRoyNoel <soham.roy.developer@gmail.com>
2021-01-22 22:46:49 +0530

    User and Admin based App User relation created

--- Server/server/src/resolvers/ProtectedResolvers/Admin/Application_User.resolver.ts
+++ Server/server/src/resolvers/ProtectedResolvers/Admin/Application_User.resolver.ts
@@ -9,60 +9,74 @@ import { Application_User_Mapper_GQLType } from '../../../entity/outputDataProvi
 @Resolver()
 export class ApplicationUserMapperResolver  {
 
-      @Query(() => [Application_User_Mapper_GQLType])
-      @UseMiddleware(IsAuthMiddleware)
-      async a_getAppsMappedToUsers(
-            @Ctx() { payload }: IctxType 
-      ) {
-            let userRole = payload!.userRole;
-                        
-            if(userRole !== 'Admin'){
-                  throw new Error('Admin Rights needed to perform this action');
+    /**
+     * Admin & User both can see the USER-APPLICATION map, 
+     * thats how USER can see their specific application &
+     * ADMIN can see all USER's all APPLICATIONs
+     * @param param0 
+    */
+    @Query(() => [Application_User_Mapper_GQLType])
+    @UseMiddleware(IsAuthMiddleware)
+    async au_getAppsMappedToUsers(
+        @Ctx() { payload }: IctxType 
+    ) {
+        let userRole = payload!.userRole;
+        let query = "";         
+        if(userRole === 'Admin'){
+            query = "select ur.Reg_UserID, ur.Reg_UserName, am.Application_ID, am.Application_Name from User_Registration ur join Application_User_Mapper aum on aum.App_user_Reg_ID = ur.Reg_UserID join Application_Master am on am.Application_ID = aum.App_Application_ID";
+        } else {
+            query = `select ur.Reg_UserID, ur.Reg_UserName, am.Application_ID, am.Application_Name from User_Registration ur join Application_User_Mapper aum on aum.App_user_Reg_ID = ur.Reg_UserID join Application_Master am on am.Application_ID = aum.App_Application_ID where ur.Reg_UserID=${payload!.uid}`;
+        }
+        
+        let gqlData: Application_User_Mapper_GQLType[] = await getManager().query(query).then((e) => {
+            return e;
+        })
+        return gqlData;
+    }  
+
+    /**
+     * Admin & User both can see the USER-APPLICATION map, 
+     * thats how USER can see their specific application &
+     * ADMIN can see all USER's all APPLICATIONs
+     * @param param0 
+    */
+    @Mutation(() => Boolean)
+    @UseMiddleware(IsAuthMiddleware)
+    async a_killOrReviveUserMappedToApp(
+        @Arg("AdminActionOverUserApplicationType") adminActionOverUserApplicationType: AdminActionOverUserApplicationType,
+        @Ctx() { payload }: IctxType 
+    ) {
+        let userRole = payload!.userRole;
+                    
+        if(userRole !== 'Admin'){
+            throw new Error('Admin Rights needed to perform this action');
+        }
+
+        // Check the state of user and app relation
+        let isActive = await Application_User_Mapper.findOne({
+            where: {
+                App_Application_ID: adminActionOverUserApplicationType.appId,
+                App_user_Reg_ID: adminActionOverUserApplicationType.userId
             }
-            let query = "select ur.Reg_UserID, ur.Reg_UserName, am.Application_ID, am.Application_Name from User_Registration ur join Application_User_Mapper aum on aum.App_user_Reg_ID = ur.Reg_UserID join Application_Master am on am.Application_ID = aum.App_Application_ID";
-            let gqlData: Application_User_Mapper_GQLType[] = await getManager().query(query).then((e) => {
-                return e;
-            })
-            return gqlData;
-      }  
+        }).then((e) => {
+            return e?.App_Map_ID_FLAG;
+        }).catch((e) => {
+            console.log("404 Err" + e);
+            return false;
+        });
 
-      @Mutation(() => Boolean)
-      @UseMiddleware(IsAuthMiddleware)
-      async a_killOrReviveUserMappedToApp(
-            @Arg("AdminActionOverUserApplicationType") adminActionOverUserApplicationType: AdminActionOverUserApplicationType,
-            @Ctx() { payload }: IctxType 
-      ) {
-            let userRole = payload!.userRole;
-                        
-            if(userRole !== 'Admin'){
-                  throw new Error('Admin Rights needed to perform this action');
-            }
-
-            // Check the state of user and app relation
-            let isActive = await Application_User_Mapper.findOne({
-                where: {
-                    App_Application_ID: adminActionOverUserApplicationType.appId,
-                    App_user_Reg_ID: adminActionOverUserApplicationType.userId
-                }
-            }).then((e) => {
-                return e?.App_Map_ID_FLAG;
-            }).catch((e) => {
-                console.log("404 Err" + e);
-                return false;
-            });
-
-            let isDone = await getConnection().createQueryBuilder().update(Application_User_Mapper)
-            .set({
-                App_Map_ID_FLAG: isActive === 1 ? 0 : 1
-            })
-            .where("App_user_Reg_ID= :uid and App_Application_ID= :appId",
-                { uid: adminActionOverUserApplicationType.userId, appId: adminActionOverUserApplicationType.appId }
-            ).execute().then((e) => {
-                return true;
-            }).catch((e) => {
-                console.log(e);
-                return false;
-            });
-            return isDone;
-      } 
+        let isDone = await getConnection().createQueryBuilder().update(Application_User_Mapper)
+        .set({
+            App_Map_ID_FLAG: isActive === 1 ? 0 : 1
+        })
+        .where("App_user_Reg_ID= :uid and App_Application_ID= :appId",
+            { uid: adminActionOverUserApplicationType.userId, appId: adminActionOverUserApplicationType.appId }
+        ).execute().then((e) => {
+            return true;
+        }).catch((e) => {
+            console.log(e);
+            return false;
+        });
+        return isDone;
+    } 
 }
